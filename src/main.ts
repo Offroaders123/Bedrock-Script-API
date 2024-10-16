9278cd97f5b77c142cd0099cf2a221508e689195
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-15 22:14:04 -0700

    Environment Debug
    Scoping out the rest of the standard library globals. Looks like in 1.21.31 BigInt isn't supported, but in 1.20.72, it's not. So I wonder if they raised the target up from ES2020.
    
    https://wiki.bedrock.dev/scripting/api-environment.html#support
    https://help.minecraft.net/hc/en-us/articles/30298767427597 (When trying the new update, looks like the Unix launcher might have to get a big rework, or it will stop working for modern versions, as they are dropping support for old Android devices, and that's what the VM runs on)
    
    So now I think we have a better idea what these types should look like! I think I will have to build a custom definitions file with all of the lib types, and to exclude the ones with the unsupported features mentioned here.

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,74 @@
-console.warn(Object.keys(console));
+Object;
+Function;
+Error;
+EvalError;
+RangeError;
+ReferenceError;
+SyntaxError;
+TypeError;
+URIError;
+InternalError;
+AggregateError;
+Array;
+Int8Array;
+Uint8Array;
+Int16Array;
+Uint16Array;
+Int32Array;
+Uint32Array;
+Float32Array;
+Float64Array;
+SharedArrayBuffer;
+ArrayBuffer;
+Uint8ClampedArray;
+parseInt;
+parseFloat;
+isNaN;
+isFinite;
+decodeURI;
+encodeURI;
+decodeURIComponent;
+encodeURIComponent;
+escape;
+unescape;
+NaN;
+Infinity;
+undefined;
+__date_clock;
+Number;
+Boolean;
+String;
+Symbol;
+Math;
+Reflect;
+// eval; (supported, has to be enabled in the manifest)
+globalThis;
+Date;
+RegExp;
+JSON;
+Proxy;
+Map;
+Set;
+WeakMap;
+WeakSet;
+DataView;
+Promise;
+console;
+console.log;
+console.warn;
+console.error;
+console.info;
 
-console.warn(Object.keys(globalThis));
+// will probably error
 
-console.warn("JSON" in globalThis);
+// Atomics; // not supported
+// BigInt; // not supported
+// BigUint64Array; // not supported
+// Intl; // not supported
+// SharedArrayBuffer; true?? nice!
 
-console.warn("Intl" in globalThis);
-
-console.warn("findLast" in Array.prototype);
-
-console.warn("Nice".at(0)!);
-
-console.warn(Promise.any([Promise.resolve(5), Promise.reject(2)]));
-
-const value = await Promise.any([Promise.resolve(5), Promise.reject(2)]);
-console.warn(value);
-
-console.warn(5n);
-
-const heyhey = [..."heyhey".matchAll(/hey/g)];
-console.warn(heyhey);
-
-(async () => {
-
-const gg = await nice();
-console.warn(gg);
-
-async function nice(): Promise<number> {
-  return 5;
-}
-
-})();
+console.warn("Atomics", "Atomics" in globalThis);
+console.warn("BigInt", "BigInt" in globalThis);
+console.warn("BigUint64Array", "BigUint64Array" in globalThis);
+console.warn("Intl", "Intl" in globalThis);
+console.warn("SharedArrayBuffer", "SharedArrayBuffer" in globalThis);
