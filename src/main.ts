48a35baaae6379c1c588a2d5e8a830e8071b8254
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-15 21:48:29 -0700

    ES2020 Target (Mostly)
    Did some debugging for feature support, and it seems to be a partial implementation around ES2020, missing BigInt support which is unfortunate.
    
    Each of these groups I ran individually on their own, so if you try to run it like this it won't work right. You can step by step run each section one at a time to see what happens.
    
    I'm glad that JSON does work though, that's good! Same with ESM itself, that's also great. I'm still curious about module resolution, and what kinds of paths it can resolve. Say like `import * as NBT from "nbtify"`, does it work with `package.json`? I think it will? And what's the thoughts towards packing your `node_modules` with the `.mcpack`? Almost undoubtedly, you'd want to bundle it into a single file, right? Or at least into minified files? Say like you use `import()` for specific features, that could be good. Actually, I should check if it supports that too.
    
    So yeah, we have `console`, and `JSON` support, and ES2020, but not including `BigInt`.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-import { system, world } from "@minecraft/server";
+console.warn(Object.keys(console));
 
-system.runInterval(() => {
-  world.sendMessage("hello world!");
-}, 40);
+console.warn(Object.keys(globalThis));
+
+console.warn("JSON" in globalThis);
+
+console.warn("Intl" in globalThis);
+
+console.warn("findLast" in Array.prototype);
+
+console.warn("Nice".at(0)!);
+
+console.warn(Promise.any([Promise.resolve(5), Promise.reject(2)]));
+
+const value = await Promise.any([Promise.resolve(5), Promise.reject(2)]);
+console.warn(value);
+
+console.warn(5n);
+
+const heyhey = [..."heyhey".matchAll(/hey/g)];
+console.warn(heyhey);
+
+(async () => {
+
+const gg = await nice();
+console.warn(gg);
+
+async function nice(): Promise<number> {
+  return 5;
+}
+
+})();
